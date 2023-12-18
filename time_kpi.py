2adfbc665ce365de6ca19573642778d11c2a232e
arnilPrateek <100081241+arnilPrateek@users.noreply.github.com>
2023-12-18 20:22:58 +0530

    Add files via upload

---
+++ time_kpi.py
@@ -0,0 +1,111 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+
+
+# UDF for calculating Total_Items
+def get_item_count(items):
+   item_count = 0
+   for i in items:
+       item_count = item_count + i[2]
+   return item_count
+
+# UDF for calculating Total_cost
+def tot_cost_per_record(items):
+   tot_cost = 0
+   for i in items:
+       tot_cost = tot_cost + (i[2] * i[3])
+   return tot_cost
+
+# UDF for calculating order type flag
+def get_order_type(type):
+    order_type = 0
+    if type == 'ORDER':
+        order_type = 1
+    else:
+        order_type = 0
+    return order_type
+
+# UDF for calculating return type flag
+def get_order_return_type(type):
+    order_return = 0
+    if type == 'ORDER':
+        order_return = 0
+    else:
+        order_return = 1
+    return order_return
+
+# Initial Spark session creation
+spark = SparkSession  \
+        .builder  \
+        .appName("StructuredSocketRead")  \
+        .getOrCreate()
+spark.sparkContext.setLogLevel('ERROR')
+
+# Reading the streaming data
+orderRaw = spark  \
+        .readStream  \
+        .format("kafka")  \
+        .option("kafka.bootstrap.servers","ec2-18-211-252-152.compute-1.amazonaws.com:9092")  \
+        .option("subscribe","real-time-project")  \
+        .load()
+
+# Define the UDFs with the utility functions
+add_total_count = udf(get_item_count, DoubleType())
+add_total_cost = udf(tot_cost_per_record, DoubleType())
+add_is_order_flg = udf(get_order_type, IntegerType())
+add_is_return_flg = udf(get_order_return_type, IntegerType())
+
+# Define schema of a single order
+jsonSchema = StructType() \
+       .add("invoice_no", StringType()) \
+       .add("country", StringType()) \
+       .add("timestamp", TimestampType()) \
+       .add("type", StringType()) \
+       .add("items", ArrayType(StructType([
+       StructField("SKU", StringType()),
+       StructField("title", StringType()),
+       StructField("unit_price", DoubleType()),
+       StructField("quantity", DoubleType())
+   ])))
+
+# Parsing the Streaming data using from_json and schema
+ord_Stream = orderRaw.select(from_json(col("value").cast("string"), jsonSchema).alias("data")).select("data.*")
+
+
+# Deriving the Required new attributes using the UDF
+DataFrame_Total_Items_Cost= ord_Stream \
+    .withColumn("Total_Items", add_total_count(ord_Stream.items)) \
+    .withColumn("Total_Cost", add_total_cost(ord_Stream.items)) \
+    .withColumn("is_order", add_is_order_flg(ord_Stream.type)) \
+    .withColumn("is_return", add_is_return_flg(ord_Stream.type)).select("invoice_no","country","timestamp","Total_Items", "Total_Cost","is_order", "is_return")
+
+# Writing the Inetermediary data into Console
+query = DataFrame_Total_Items_Cost  \
+        .writeStream  \
+        .outputMode("append")  \
+        .format("console")  \
+        .option("truncate", "false") \
+        .start()
+
+# Calculate time based KPIs
+aggStreamByTime = DataFrame_Total_Items_Cost \
+    .withWatermark("timestamp", "1 minute") \
+    .groupBy(window("timestamp", "1 minute", "1 minute")) \
+    .agg(sum("Total_Cost").alias("total_volume_of_sales"),count("invoice_no").alias("OPM"),avg("is_return").alias("avg_rate_of_return")).select("window", "OPM", "total_volume_of_sales", "avg_rate_of_return")
+
+
+
+
+# Writing the Time Based KPIs into HDFS
+queryByTime= aggStreamByTime.writeStream \
+        .format("json") \
+        .outputMode("append") \
+        .option("truncate", "false") \
+        .option("path", "time-wise-data") \
+        .option("checkpointLocation", "time-cp1") \
+        .trigger(processingTime="1 minute") \
+        .start()
+
+
+queryByTime.awaitTermination()
